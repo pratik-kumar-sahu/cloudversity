526b9c58f56a7c034af2148d0b3eb17d273377fb
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-05 16:41:17 +0530

    Initial setup, making ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container__leftContainer"></div>
+      <div className="container__mainContainer"></div>
+      <div className="container__rightContainer"></div>
+    </div>
+  );
+}
+
+export default App;
