4f5a3ee209557a1dbfb308f1a6bc9de9ccd70653
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-07 22:15:28 +0530

    Added images and setup done with main container

--- src/App.js
+++ src/App.js
@@ -1,77 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LeftContainer from "./containers/LeftContainer";
+import RightContainer from "./containers/RightContainer";
+import Home from "./pages/Home/Home";
 import "./App.scss";
-import home from "./home.png";
-import menu from "./menu.png";
-import c from "./c.png";
-import prev from "./prev.png";
-import next from "./next.png";
-import profile from "./profile.png";
-import logout from "./logout.png";
-import Course from "./components/Course";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="container">
-      <div className="container__leftContainer">
-        <img className="container__leftContainer--icon" src={c} alt="c-icon" />
-        <img
-          className="container__leftContainer--icon"
-          src={home}
-          alt="home-icon"
-        />
-        <img
-          className="container__leftContainer--icon"
-          src={menu}
-          alt="menu-icon"
-        />
-        <img
-          className="container__leftContainer--icon"
-          src={profile}
-          alt="profile-icon"
-        />
-        <img
-          className="container__leftContainer--icon"
-          src={logout}
-          alt="logout-icon"
-        />
-      </div>
-      <div className="container__mainContainer">
-        <div className="container__mainContainer-welcome">
-          Welcome to Dashboard
-        </div>
-        <div className="container__mainContainer-watching">
-          <Course />
-          <img
-            className="container__mainContainer-watching--icon1"
-            src={prev}
-            alt="previous"
-          />
-          <img
-            className="container__mainContainer-watching--icon2"
-            src={next}
-            alt="next"
-          />
-        </div>
-        <div className="container__mainContainer-courseList">
-          {/* <h3>Courses</h3> */}
-          <div>
-            <span>All Courses</span>
-            <span>Most Popular</span>
-            <span>Top Rated</span>
-            <span>Newest</span>
-          </div>
-          <div className="container__mainContainer-courseList--scroll">
-            <Course />
-            <Course />
-            <Course />
-            <Course />
-            <Course />
-            <Course />
-            <Course />
-            <Course />
-          </div>
-        </div>
-      </div>
-      <div className="container__rightContainer"></div>
+    <div className="app">
+      <Router>
+        <LeftContainer />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+        <RightContainer />
+      </Router>
     </div>
   );
 }
