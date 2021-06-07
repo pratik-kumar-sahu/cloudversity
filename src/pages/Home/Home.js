a85aa88379fbc9c4895fd4a06bff0de6a6d8f3c2
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-07 22:16:45 +0530

    Added pages for different routes

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import CourseCard from "../../components/CourseCard";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__scroll">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+      <div className="home__scroll">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
