b8a92c5e89331081c99b26c52a0988b4775b76a7
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-07 22:16:24 +0530

    Added container and routes

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainContainer() {
+  return <div className="mainContainer"></div>;
+}
+
+export default MainContainer;
---
+++ src/containers/RightContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RightContainer() {
+  return <div className="rightContainer"></div>;
+}
+
+export default RightContainer;
