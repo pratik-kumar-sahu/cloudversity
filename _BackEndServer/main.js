95efe9524bc8284a48d073ac56a6d3df0e5d56c0
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-10 22:43:24 +0530

    Added other files

---
+++ _BackEndServer/main.js
@@ -0,0 +1,55 @@
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 9000;
+const cors = require("cors");
+const MongoInit = require("./Config/Mongo");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const studentRoutes = require("./Routes/student");
+const tutorRoutes = require("./Routes/tutor");
+const courseRoutes = require("./Routes/course");
+
+const app = express();
+
+//-------- Connecting to DB --------- //
+MongoInit();
+
+//-------- Cookie and Session setup --------- //
+app.use(cookieParser());
+app.use(
+  session({
+    // It holds the secret key for session
+    secret: "My_Secret_Key", // need to replace it with process.env
+
+    // Forces the session to be saved
+    // back to the session store
+    resave: true,
+
+    // Forces a session that is "uninitialized"
+    // to be saved to the store
+    saveUninitialized: true,
+  })
+);
+
+//-------- Setting Up static folder --------- //
+app.use(express.static("public"));
+//-------- Setting up Body Parser --------- //
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+//-------- Using CORS middleware --------- //
+app.use(cors());
+
+//-------- Importing and Using the Routes --------- //
+app.use("/stu", studentRoutes);
+app.use("/tut", tutorRoutes);
+app.use("/tut", courseRoutes);
+
+//-------- Route to test Sever Health --------- //
+app.get("/", (req, res) => {
+  res.send("Server is healthy, Health Check Passed!");
+});
+
+//-------- Listening to the PORT --------- //
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`);
+});
